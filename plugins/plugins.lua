045111a60a42c430f0bfb7b1e3dbb527d821cc34
Sajad Aliraqe <sajad@bareed.ws>
2016-06-12 13:01:16 +0300

    By Sajad Aliraqe

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -122,7 +122,7 @@ end
 
 local function run(msg, matches)
   -- Show the available plugins 
-  if matches[1] == '!plugins' then
+  if matches[1] == '/plugins' then
     return list_plugins()
   end
 
